7a3680fef19f54257457ef115fc0bb7ef8c5c1dd
Mahmoud Hamdy <houdahamdy12@gmail.com>
2022-11-08 16:11:13 +0200

    w

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div>2022 <span><Link to='/' className='span'>Online Store</Link></span> All rights reserved</div>
-      <div>Made by <a href='https://mahmoudhamdy-portfolio.netlify.app/'>Mahmoud Hamdy</a></div>
+      <div>Made by <a href='https://mahmoudhamdy-portfolio.netlify.app/'> Mahmoud Hamdy</a></div>
     </div>
   )
 }
